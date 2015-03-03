72086a2ca03779a6bedf9301e29b6b9d4b84824f
Adam Bien <abien@adam-bien.com>
2015-03-03 13:45:45 +0100

    Control layer introduced

--- hello/src/main/java/at/raiffeisenbank/customer/boundary/CustomerManager.java
+++ hello/src/main/java/at/raiffeisenbank/customer/boundary/CustomerManager.java
@@ -1,7 +1,9 @@
 package at.raiffeisenbank.customer.boundary;
 
+import at.raiffeisenbank.customer.control.CustomerValidator;
 import at.raiffeisenbank.customer.entity.Customer;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.interceptor.Interceptors;
 
 /**
@@ -12,7 +14,14 @@ import javax.interceptor.Interceptors;
 @Interceptors({InvocationAudit.class})
 public class CustomerManager {
 
+    @Inject
+    CustomerValidator cv;
+
     public Customer get(long id, String name, boolean active) {
-        return new Customer(id, name, active);
+        Customer customer = new Customer(id, name, active);
+        if (!cv.isValid(customer)) {
+            throw new IllegalStateException("Invalid!");
+        }
+        return customer;
     }
 }
---
+++ hello/src/main/java/at/raiffeisenbank/customer/control/CustomerValidator.java
@@ -0,0 +1,15 @@
+package at.raiffeisenbank.customer.control;
+
+import at.raiffeisenbank.customer.entity.Customer;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class CustomerValidator {
+
+    public boolean isValid(Customer customer) {
+        return true;
+    }
+
+}
