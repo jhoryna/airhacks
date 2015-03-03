e5841c8a883b34edfc668fe95f25095bcbfcd637
Adam Bien <abien@adam-bien.com>
2015-03-03 11:37:42 +0100

    Simplistic GET / POST is working

---
+++ hello/src/main/java/at/raiffeisenbank/customer/boundary/CustomersResource.java
@@ -0,0 +1,39 @@
+package at.raiffeisenbank.customer.boundary;
+
+import java.net.URI;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Path("customers")
+public class CustomersResource {
+
+    @GET
+    public String all() {
+        return "duke,nuke";
+    }
+
+    @GET
+    @Path("{id}")
+    public String customer(@PathParam("id") long id) {
+        return "duke with: " + id;
+    }
+
+    @POST
+    public Response save(@Context UriInfo info, String customer) {
+        System.out.println("customer = " + customer);
+        URI uri = info.getAbsolutePathBuilder().
+                path("/" + System.currentTimeMillis()).
+                build();
+        return Response.created(uri).build();
+    }
+
+}
