77ba0d45e2307fd3f25ffb8cb97c8714ab670af5
Adam Bien <abien@adam-bien.com>
2015-03-03 15:48:55 +0100

    JPA query test

--- hello/src/test/java/at/raiffeisenbank/customer/entity/CustomerIT.java
+++ hello/src/test/java/at/raiffeisenbank/customer/entity/CustomerIT.java
@@ -1,8 +1,11 @@
 package at.raiffeisenbank.customer.entity;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertThat;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -28,6 +31,17 @@ public class CustomerIT {
         this.tx.begin();
         this.em.persist(new Customer(42, "hugo", true));
         this.tx.commit();
+    }
+
+    @Test
+    public void allQuery() {
+        this.tx.begin();
+        final Customer expected = new Customer(System.currentTimeMillis(), "hugo", true);
+        this.em.persist(expected);
+        this.tx.commit();
+
+        List<Customer> customers = this.em.createNamedQuery("all", Customer.class).getResultList();
+        assertThat(customers, hasItem(expected));
     }
 
 }
