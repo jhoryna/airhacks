c7e9ef4667af03bf238ce95e4c3585a817aac23b
Adam Bien <abien@adam-bien.com>
2015-03-04 09:50:16 +0100

    Pipeline implemented with completable future

---
+++ basics/src/main/java/at/raiffeisenbank/sample/MessageProvider.java
@@ -0,0 +1,13 @@
+package at.raiffeisenbank.sample;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class MessageProvider {
+
+    public String message() {
+        return "function, async morning from pipeline";
+    }
+
+}
--- basics/src/main/java/at/raiffeisenbank/sample/Messenger.java
+++ basics/src/main/java/at/raiffeisenbank/sample/Messenger.java
@@ -1,8 +1,13 @@
 package at.raiffeisenbank.sample;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 
 /**
  *
@@ -11,8 +16,15 @@ import javax.enterprise.event.Observes;
 @Stateless
 public class Messenger {
 
+    @Inject
+    MessageProvider provider;
+
+    @Resource
+    ManagedExecutorService mes;
+
     public void hello(@Observes Consumer<String> sink) {
-        sink.accept("hello disfunctional morning");
+        Supplier<String> supplier = provider::message;
+        CompletableFuture.supplyAsync(supplier, mes).thenAccept(sink);
     }
 
 }
