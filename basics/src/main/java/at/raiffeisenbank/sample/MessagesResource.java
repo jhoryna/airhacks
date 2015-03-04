b5ac22eab315bf0519112311f8b9778aa5610353
Adam Bien <abien@adam-bien.com>
2015-03-04 09:44:59 +0100

    Events with Java 8

--- basics/src/main/java/at/raiffeisenbank/sample/MessagesResource.java
+++ basics/src/main/java/at/raiffeisenbank/sample/MessagesResource.java
@@ -1,5 +1,8 @@
 package at.raiffeisenbank.sample;
 
+import java.util.function.Consumer;
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.container.AsyncResponse;
@@ -12,9 +15,13 @@ import javax.ws.rs.container.Suspended;
 @Path("messages")
 public class MessagesResource {
 
+    @Inject
+    Event<Consumer<String>> consumers;
+
     @GET
     public void all(@Suspended AsyncResponse response) {
-        response.resume("hey joe");
+        Consumer<String> consumer = response::resume;
+        consumers.fire(consumer);
 
     }
 
--- basics/src/main/java/at/raiffeisenbank/sample/Messenger.java
+++ basics/src/main/java/at/raiffeisenbank/sample/Messenger.java
@@ -1,6 +1,8 @@
 package at.raiffeisenbank.sample;
 
+import java.util.function.Consumer;
 import javax.ejb.Stateless;
+import javax.enterprise.event.Observes;
 
 /**
  *
@@ -9,8 +11,8 @@ import javax.ejb.Stateless;
 @Stateless
 public class Messenger {
 
-    public String hello() {
-        return "good morning";
+    public void hello(@Observes Consumer<String> sink) {
+        sink.accept("hello disfunctional morning");
     }
 
 }
